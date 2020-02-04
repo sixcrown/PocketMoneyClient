589f01ff53b0bdfe85a80b12e9490307708be190
sixcrown <kartoszbawalkiewicz@gmail.com>
2020-02-04 20:26:40 +0100

    dalej nad menu użytkownika pracę trwają

--- src/app/user/get-my-children/get-my-children.component.ts
+++ src/app/user/get-my-children/get-my-children.component.ts
@@ -2,19 +2,39 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthService } from './../../shared/auth.service'
 import {Children} from '../../entities/Children'
+import { FormBuilder, FormGroup } from "@angular/forms";
 @Component({
   selector: 'app-get-my-children',
   templateUrl: './get-my-children.component.html',
   styleUrls: ['./get-my-children.component.css']
 })
 export class GetMyChildrenComponent implements OnInit {
-
+  signinForm: FormGroup;
   showDetails:boolean = false
   addChildrenClicker:boolean = false
   indexClicked:string
   childClicker:boolean = false
-  constructor(public authService: AuthService,private http: HttpClient) { }
+  sex = []
+  constructor(public authService: AuthService,private http: HttpClient,    public fb: FormBuilder,) 
+{    
+    this.signinForm = this.fb.group(
+      {
+    pocketMoney: [''],
+    description:[''],
+    sex:[''],
+    isLivingWithParents:false,
+    administrationUnit:[''],
+    educationLevel:['']
+      })
+    this.sex = this.getSexs()
+    this.signinForm.controls.sex.patchValue(this.sex[0].id)
+    // of(this.getSex()).subscribe(sex => {
+    //     this.sex = sex;
+    //     this.signinForm.controls.orders.patchValue(this.sex[0].id);
+    //   });
+}
   readonly urlGetChildren = 'http://localhost:8080/api/getMyChildren';
+  readonly urlAddChild = 'http://localhost:8080/api/addChild';
   Childrens:Children[]
   ngOnInit() {
     this.getMyChildren()
@@ -43,9 +63,22 @@ export class GetMyChildrenComponent implements OnInit {
   {
 
   }
-  submit()
+  submitNewKid()
   {
-
+    this.addChildrenClicker = false;
+    //console.log(this.signinForm.value)
+    //console.log(this.signinForm.get('isLivingWithParents'));
+    this.http.post(this.urlAddChild,this.signinForm.value).subscribe((data:any)=>{
+    (error)=>console.log(error)});
+  }
+  getSexs() {
+    return [
+      { id: '1', name: 'order 1' },
+      { id: '2', name: 'order 2' },
+      { id: '3', name: 'order 3' },
+      { id: '4', name: 'order 4' }
+    ];
   }
 
+  
 }
