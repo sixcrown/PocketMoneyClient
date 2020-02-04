2b2cd61d6252055281346a8296c652dc31297ecb
sixcrown <kartoszbawalkiewicz@gmail.com>
2020-02-04 17:01:22 +0100

    w trakcie przerobki get my children component

--- src/app/user/get-my-children/get-my-children.component.ts
+++ src/app/user/get-my-children/get-my-children.component.ts
@@ -9,18 +9,43 @@ import {Children} from '../../entities/Children'
 })
 export class GetMyChildrenComponent implements OnInit {
 
+  showDetails:boolean = false
+  addChildrenClicker:boolean = false
+  indexClicked:string
+  childClicker:boolean = false
   constructor(public authService: AuthService,private http: HttpClient) { }
-  readonly url = 'http://localhost:8080/api/getMyChildren';
+  readonly urlGetChildren = 'http://localhost:8080/api/getMyChildren';
   Childrens:Children[]
   ngOnInit() {
     this.getMyChildren()
   }
   getMyChildren()
   {
-    this.http.get(this.url).subscribe((data:any)=>{
+    this.http.get(this.urlGetChildren).subscribe((data:any)=>{
       this.Childrens=data
       console.log(this.Childrens)
     });
+  }
+  addChildrenClick()
+  {
+    if(this.addChildrenClicker) this.addChildrenClicker = false
+    else this.addChildrenClicker=true
+    this.getMyChildren()
+  }
+  ChildClick(id)
+  {
+    this.childClicker=true;
+    console.log("UGABUGA");
+    this.indexClicked = id
+    console.log(this.indexClicked)
+  }
+  ChildDelete()
+  {
+
+  }
+  submit()
+  {
+
   }
 
 }
