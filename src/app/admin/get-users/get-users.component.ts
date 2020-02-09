90441615bb2bdcdae3c8691212149deee00bb45b
sixcrown <kartoszbawalkiewicz@gmail.com>
2020-02-09 20:02:22 +0100

    oki done

--- src/app/admin/get-users/get-users.component.ts
+++ src/app/admin/get-users/get-users.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient,HttpHeaders} from '@angular/common/http';
+import {User} from '../../shared/user'
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-get-users',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./get-users.component.css']
 })
 export class GetUsersComponent implements OnInit {
-
-  constructor() { }
+  users:User[]
+  headers = new HttpHeaders().set('Content-Type', 'application/json');
+  readonly urlGetUsers = 'http://localhost:8080/api/getDefaultUsers';
+  readonly urlDeleteUser = 'http://localhost:8080/api/deleteUser'
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.userGet()
   }
-
+  userGet()
+  {
+    this.http.get(this.urlGetUsers).subscribe((data:any)=>
+    {
+      console.log(data)
+      this.users = data;
+    })
+  }
+  Userdelete(id)
+  { 
+      console.log(id)
+      this.http.delete(this.urlDeleteUser + '/' + id, { headers: this.headers })
+      .subscribe((res: any) =>
+       {
+        this.userGet()
+       })
+  
+    
+  }
 }
--- src/app/shared/user.ts
+++ src/app/shared/user.ts
@@ -1,4 +1,5 @@
 export class User {
+    id:any
     username:String
     password:String
      email:String
