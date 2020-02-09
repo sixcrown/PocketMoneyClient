7e0f2ec30686a057d0adb9420a37a0843958343d
sixcrown <kartoszbawalkiewicz@gmail.com>
2020-02-09 19:17:02 +0100

    xdd panowie cmon

--- src/app/admin/get-children/get-children.component.ts
+++ src/app/admin/get-children/get-children.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Children} from '../../entities/Children'
+import { HttpClient,HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-get-children',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./get-children.component.css']
 })
 export class GetChildrenComponent implements OnInit {
-
-  constructor() { }
+  headers = new HttpHeaders().set('Content-Type', 'application/json');
+  Childrens:Children[]
+  readonly urlGetChildren = 'http://localhost:8080/api/getAllChildren';
+  readonly urlDeleteChild = 'http://localhost:8080/api/deleteChild';
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getMyChildren()
   }
+  getMyChildren()
+  {
+    this.http.get(this.urlGetChildren).subscribe((data:any)=>{
+      this.Childrens=data
+    });
+  }
+  ChildDelete(id)
+  {
+    console.log(id)
+    this.http.delete(this.urlDeleteChild + '/' + id, { headers: this.headers })
+    .subscribe((res: any) =>
+     {
+      this.getMyChildren()
+     })
 
+  }
 }
